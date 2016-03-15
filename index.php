c283f9d2f795d974ee031c759cd3c05d569084bd
tdzoca <tdzpatriota@gmail.com>
2016-03-15 20:59:18 -0300

    add boostrap e tela de login

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-<?php
+<?php include './style/template.php'; ?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="col-sm-3 col-sm-offset-3" style="margin-top: 250px;">
+    <h2>Disco Redords PHP</h2> .. <h5>Login</h5>
+    <input name="email" class="form-control">
+    <h5>Senha</h5>
+    <input name="senha" class="form-control">
+    <br>
+    <input type="submit" name="logar" class="btn btn-success" value="entrar">
+    <input type="reset" name="redefinir" class="btn btn-danger" value="limpar">     
+</div>    
+    
